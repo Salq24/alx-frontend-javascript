e1b6de4178b7d4222a96f7df55bdda991509c71e
Queen <qogheneochuko@gmail.com>
2024-10-03 20:49:47 +0000

    calsses

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -7,6 +7,7 @@ export default class HolbertonCourse {
     } else if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
     }
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -15,12 +16,15 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   get length() {
     return this._length;
   }
+
   get students() {
     return this._students;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
