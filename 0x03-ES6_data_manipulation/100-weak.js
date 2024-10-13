9ebfdbe9a17f796f5eb1596d00e671ace1259faf
Queen <qogheneochuko@gmail.com>
2024-10-13 17:49:14 +0000

    data

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,11 @@
 export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
-  const count = weakMap.get(endpoint) || 0;
-  if (count >= 5) {
-    throw new Error('Endpoint load is high');
-  }
+  let cnt = weakMap.get(endpoint) || 0;
+  cnt += 1;
 
- weakMap.set(endpoint, count + 1);
-}
+  if (cnt >= 5) {
+    throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, cnt);
+  }
 }
